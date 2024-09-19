f9fb34355078754d7a4fcb15daa02204b6267cd8
ana maria <anamaria62003@gmail.com>
2024-09-19 16:40:00 -0500

    esta repositorio es para hacer ejemplo de cadenas en pycharm

--- ejemplocadena/main.py
+++ ejemplocadena/main.py
@@ -29,3 +29,42 @@ title ="Cien Años De Soledad"
 titleConvert = title.casefold()
 print(titleConvert)
 
+#string center()
+fruit ="banana"
+textCenter = fruit.center(20, "-")
+print(textCenter)
+
+#string count()
+title1 = "i love apple, apple are my favorite fruit"
+result2 = title1.count("apple")
+print(result2)
+
+#string endswith()
+text6 ="Curso, fundamentos con python."
+result3 = text6.endswith(".")
+print(result3)
+
+#string expandtabs()
+letter ="F\tU\tP"
+letterSpace = letter.expandtabs(2)
+print(letterSpace)
+
+#string find}
+text7 = "Hola, bienvenidos a colombia"
+result4 =text7.find("bienvenidos")
+print(result4)
+
+#funcion title
+text8 = "welcome  to my word"
+result5 = text8.title()
+print(result5)
+
+#funcion isalnum
+alphanumeric ="python312"
+result6 = alphanumeric.isalnum()
+print(result6)
+
+#funcion isalpha
+letters = "Space X"
+result7= letters.isalpha()
+print(result7)
