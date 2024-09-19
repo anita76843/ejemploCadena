1d30f9798cc9062fe92e1bab8fe7ed2c47680301
ana maria <anamaria62003@gmail.com>
2024-09-19 16:20:06 -0500

    esta repositorio es para hacer ejemplo de cadenas en pycharm

---
+++ ejemplocadena/main.py
@@ -0,0 +1,31 @@
+#string concatenar
+text1 ="fundamentos con "
+text2 = "Python"
+#test3 = 5
+result = text1 + text2
+print (result)
+
+name = "ana"
+lastName =" lopez"
+fullName = name + "" +lastName
+print(fullName)
+
+#format strimg
+price = 97
+text3 =f"the price is {price:.2f} dollars"
+print(text3)
+
+#math operation
+text4 = f"la multiplicacion es {20*59}"
+print(text4)
+
+#string capitalize
+text5 = "python es un lenguaje de alto nivel de programacion"
+result1 =text5.capitalize()
+print(result1)
+
+#string casefold()
+title ="Cien Años De Soledad"
+titleConvert = title.casefold()
+print(titleConvert)
+
